24385b7f934f0bb146497485d7fee4520c11c8f3
MohamedGoush <0x1110111@gmail.com>
2023-10-28 15:29:23 +0530

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by python on 28/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
